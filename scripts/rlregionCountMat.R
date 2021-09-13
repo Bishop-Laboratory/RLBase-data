150bc24a3bde3a56eab29c65c231863af0aa8789
millerh1 <millerh1@livemail.uthscsa.edu>
2021-09-13 17:30:07 -0500

    ready for rlcounts

--- scripts/rlregionCountMat.R
+++ scripts/rlregionCountMat.R
@@ -1,6 +1,7 @@
 #' Script for compiling latest RLBase data for RLHub
 library(tidyverse)
 library(RLSeq)
+library(Rsubread)
 library(pbapply)
 pbo <- pboptions(type="txt") 
 
@@ -12,7 +13,8 @@ pbo <- pboptions(type="txt")
 BW_FOLDER <- "rlbase-data/rlpipes-out/coverage/"
 
 # Number of cores to use for parallel operations
-CORES_TO_USE <- 6
+args <- commandArgs(trailingOnly = TRUE)
+CORES <- args[1]
 
 #####################
 
@@ -25,6 +27,7 @@ rlrs <- lapply(opts, function(opt) {
   # Wrangle granges from rlregions table
   gr <- paste0("rlbase-data/misc/rlregions_", opt, "_table.tsv") %>%
     read_tsv(show_col_types = FALSE, progress = FALSE) %>%
+    filter(! is_repeat) %>%
     select(rlregion, location) %>%
     mutate(seqnames = gsub(location, pattern = gpat, replacement = "\\1"),
            start = as.numeric(gsub(location, pattern = gpat, replacement = "\\2")),
@@ -32,64 +35,49 @@ rlrs <- lapply(opts, function(opt) {
            strand =  gsub(location, pattern = gpat, replacement = "\\4")) %>%
     select(-location) %>%
     as.data.frame() %>%
-    GenomicRanges::makeGRangesFromDataFrame()
+    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = TRUE)
 }) 
 names(rlrs) <- opts
-rlru <- GenomicRanges::union(rlrs$dRNH, rlrs$S96) %>%
-  GenomicRanges::reduce() %>%
-  unique()
+combo <- c(rlrs$dRNH, rlrs$S96)
+types <- gsub(combo$rlregion, pattern = "(.+)_.+", replacement = "\\1")
+rvmp <- combo %>%
+  GenomicRanges::reduce(with.revmap=TRUE) 
+labs <- parallel::mclapply(seq(rvmp$revmap), function(i) {
+  x <- rvmp$revmap[[i]]
+  paste0(unique(types[x]), collapse = " ")
+}, mc.cores = CORES)
+rlregions_union <- rvmp %>%
+  unique() %>%
+  tibble::as_tibble() %>%
+  mutate(rlrID = paste0("OL_RLRegion_", seq(seqnames)),
+         sources = unlist(!! labs),
+         sources = case_when(
+           sources == "S96 dRNH" ~ "dRNH S96",
+           TRUE ~ sources
+         )) %>%
+  select(-revmap) %>%
+  relocate(rlrID, .before = seqnames) 
+save(rlregions_union, file = "rlbase-data/misc/rlregions_union.rda", compress = "xz")
+save(rlregions_union, file = "misc-data/rlhub/rlregions/rlregions_union.rda", compress = "xz")
 
-# List the available bw files and wrangle
-# Alternatively just download them
-bwFiles <- list.files(BW_FOLDER, full.names = TRUE)
-bws <- tibble::tibble(
-  bw = gsub(bwFiles, pattern = ".+([ES]{1}RX[0-9]+.+)$", replacement = "\\1")
-) %>%
-  dplyr::mutate(id = gsub(.data$bw, pattern = "(.+)_(.+)\\.bw",
-                          replacement = "\\1"),
-                genome = gsub(.data$bw, pattern = "(.+)_(.+)\\.bw",
-                              replacement = "\\2")) %>%
-  dplyr::filter(genome == "hg38")
 
-# Extract the coverage across the gs region windows
-# NOTE: this took a long time to finish and segfaulted several times
-# This is due to issues in the UCSC bigWig accessors in the rtracklayer 
-# package and SSL timeouts. Maybe be easier to just download locally.
-resLst <- parallel::mclapply(seq(bws$id)[1], function(rowNow) {
-  
-  # Get current values
-  idNow <- bws$id[rowNow]
-  bwFile <- bws$bw[rowNow]
-  
-  # Read in the bigWig file using these locations
-  bwCon <- rtracklayer::BigWigFile(
-    paste0(BW_FOLDER, bwFile)
-  )
-  bw <- rtracklayer::import.bw(con = bwCon, 
-                               selection = rlru)
-  
-  # Wrangle BW into tibble
-  bw <- bw %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(chrom = .data$seqnames) %>%
-    dplyr::mutate(chrom = as.character(.data$chrom))
-  
-  # Summarize across gs intervals with valr
-  valr::bed_map(x = hg38wds, y = bw, value = sum(score)) %>%
-    dplyr::mutate(location = paste0(
-      .data$chrom, "_", .data$start, "_", .data$end
-    ),
-    id = !! idNow) %>%
-    dplyr::select(.data$location, .data$id, .data$value) 
-  
-}, mc.cores = 44) 
+# Get seq info
+samps <- read_tsv("rlbase-data/rlbase_samples.tsv") %>%
+  filter(genome == "hg38", group == "rl") %>%
+  select(experiment, condType, strand_specific, paired_end) %>%
+  unique() %>%
+  mutate(bam = paste0("rlbase-data/rlpipes-out/bam/", experiment, "/", experiment, "_hg38.bam"),
+         bam_avail = file.exists(bam)) %>%
+  filter(bam_avail)
 
-# Wrangle
-gsSignalRLBase <- dplyr::bind_rows(resLst) %>%
-  tidyr::pivot_wider(id_cols = .data$location,
-                     names_from = .data$id,
-                     values_from = .data$value)
+# Use RSubRead to get the counts within each
+fcRes <- featureCounts(
+  samps$bam, 
+  annot.ext =  rename(rlregions_union, Chr = seqnames, GeneID = rlrID,
+                      Start = start, End = end, Strand = strand),
+  allowMultiOverlap = TRUE,  # Reads may span multiple rlregions easily
+  minMQS = 10,
+  isPairedEnd = samps$paired_end
+)
+save(fcRes, file = "tmp/fcRes.rda")
 
-############################
-
-save(gsSignalRMapDB, file = "misc-data/gsSignalRLBase.rda", compress = "xz")
